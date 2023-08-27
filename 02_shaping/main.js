604def8d30cd3e29749bb7d25f3a8fd655fc4630
kyoko-nr <kyoko_kitagawa@outlook.jp>
2023-08-27 16:19:50 +0900

    added shaping page

--- 02_shaping/main.js
+++ 02_shaping/main.js
@@ -5,8 +5,8 @@ import { handleMouse } from "../utils/three/handleMouse";
 
 const vshader = `
   void main() {
-    gl_Position = vec4( position, 1.0 );
-    // gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 1.0, 1.0);
+    // gl_Position = vec4( position, 1.0 );
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 1.0, 1.0);
   }
 `;
 
@@ -15,10 +15,22 @@ const fshader = `
   uniform vec2 u_mouse;
   uniform vec2 u_resolution;
 
+  float plot(vec2 st) {
+    // y = x の関数グラフ
+    // abs(st.y - st.x)が0.0以上0.01以下の場合に0.0〜1.0の値を返す
+    return smoothstep(0.01, 0.0,abs(st.y - st.x));
+  }
+
   void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    vec2 mouse = u_mouse / u_resolution.xy;
-    gl_FragColor = vec4(mouse.x, mouse.y, abs(sin(u_time)), 1.0);
+    vec3 color = vec3(st.x);
+    // Plot a line
+    float pct = plot(st);
+    color = (1.0 - pct) * color + pct * vec3(0.0,1.0,0.0);
+    // color = pct * color + pct * vec3(0.0, 1.0, 0,0);
+    // color = 1.0 - pct * vec3(0.0, 1.0, 0.0);
+
+    gl_FragColor = vec4(color,1.0);
   }
 `;
 
