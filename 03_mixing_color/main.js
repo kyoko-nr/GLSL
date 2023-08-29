977ed0f49a9720385ecf1b0ca0a9bd8ea0d0d620
kyoko-nr <kyoko_kitagawa@outlook.jp>
2023-08-29 09:20:02 +0900

    modify mix color

--- 03_mixing_color/main.js
+++ 03_mixing_color/main.js
@@ -14,20 +14,55 @@ const fshader = `
   uniform vec2 u_mouse;
   uniform vec2 u_resolution;
 
-  float plot(vec2 st) {
-    // y = x の関数グラフ
-    // abs(st.y - st.x)が0.0以上0.01以下の場合に0.0〜1.0の値を返す
-    return smoothstep(0.01, 0.0,abs(st.y - st.x));
+  vec3 colorA = vec3(0.458, 0.615, 0.767);
+  vec3 colorB = vec3(0.925, 0.933, 0.505);
+
+  float PI = 3.14159265359;
+
+  float easeInOutQuint(float x) {
+    return x < 0.5 ? 16.0 * x * x * x * x * x : 1.0 - pow(-2.0 * x + 2.0, 5.0) / 2.0;
+    }
+
+  float easeOutExpo(float x) {
+    return x == 1.0 ? 1.0 : 1.0 - pow(2.0, -10.0 * x);
+  }
+
+  float easeInOutBack(float x) {
+    float c1 = 1.70158;
+    float c2 = c1 * 1.525;
+
+    return x < 0.5
+    ? (pow(2.0 * x, 2.0) * ((c2 + 1.0) * 2.0 * x - c2)) / 2.0
+    : (pow(2.0 * x - 2.0, 2.0) * ((c2 + 1.0) * (x * 2.0 - 2.0) + c2) + 2.0) / 2.0;
+  }
+
+  float plot (vec2 st, float pct){
+    return  smoothstep( pct-0.05, pct, st.y) -
+            smoothstep( pct, pct+0.05, st.y);
   }
 
   void main() {
-    vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    vec3 color = vec3(st.x);
-    // Plot a line
-    float pct = plot(st);
-    color = (1.0 - pct) * color + pct * vec3(0.0,1.0,0.0);
-    // color = pct * color + pct * vec3(0.0, 1.0, 0,0);
-    // color = 1.0 - pct * vec3(0.0, 1.0, 0.0);
+    // vec3 color = vec3(0.0);
+    // float percentage = easeInOutQuint(abs(sin(u_time)));
+    // color = mix(colorA, colorB, percentage);
+
+    // gl_FragColor = vec4(color,1.0);
+
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(0.0);
+
+    vec3 pct = vec3(st.x);
+
+    pct.r = smoothstep(0.0, 1.0, st.x);
+    pct.g = sin(st.x*PI);
+    pct.b = easeOutExpo(st.x);
+
+    color = mix(colorA, colorB, pct);
+
+    // Plot transition lines for each channel
+    color = mix(color,vec3(1.0,0.0,0.0),plot(st,pct.r));
+    color = mix(color,vec3(0.0,1.0,0.0),plot(st,pct.g));
+    color = mix(color,vec3(0.0,0.0,1.0),plot(st,pct.b));
 
     gl_FragColor = vec4(color,1.0);
   }
