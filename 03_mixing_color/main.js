bd1675c85451a4b043dba030777f3f2dac2bb051
kyoko-nr <kyoko_kitagawa@outlook.jp>
2023-08-30 09:55:57 +0900

    added raindow

--- 03_mixing_color/main.js
+++ 03_mixing_color/main.js
@@ -41,30 +41,63 @@ const fshader = `
             smoothstep( pct, pct+0.05, st.y);
   }
 
-  void main() {
-    // vec3 color = vec3(0.0);
-    // float percentage = easeInOutQuint(abs(sin(u_time)));
-    // color = mix(colorA, colorB, percentage);
+  float plotLower (vec2 st, float percentage) {
+    return smoothstep( percentage + 0.5, percentage - 1.5, st.y);
+  }
 
-    // gl_FragColor = vec4(color,1.0);
+  float plotUpper (vec2 st, float percentage) {
+    return smoothstep( percentage - 0.5, percentage, st.y);
+  }
 
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    vec3 color = vec3(0.0);
+  vec4 drawTurner (vec2 st, float u_time) {
+    vec3 beige = vec3(0.87,0.81,0.66);
+    vec3 blue = vec3(0.33,0.46,0.69);
+    vec3 red = vec3(0.74,0.32,0.07);
+
+    vec3 color = beige;
 
     vec3 pct = vec3(st.x);
 
-    pct.r = smoothstep(0.0, 1.0, st.x);
+    float amountR = abs(sin(u_time * 0.1));
+    float amountB = 1.0 - amountR;
+
+    pct.r = smoothstep(0.0, 1.0, amountR + st.x);
     pct.g = sin(st.x*PI);
-    pct.b = easeOutExpo(st.x);
+    pct.b = easeOutExpo(amountB + st.x);
 
-    color = mix(colorA, colorB, pct);
+    color = mix(color,beige,plotLower(st,pct.g));
+    color = mix(color,blue,plotUpper(st,pct.b));
+    color = mix(color,red,plotLower(st,pct.r));
 
-    // Plot transition lines for each channel
-    color = mix(color,vec3(1.0,0.0,0.0),plot(st,pct.r));
-    color = mix(color,vec3(0.0,1.0,0.0),plot(st,pct.g));
-    color = mix(color,vec3(0.0,0.0,1.0),plot(st,pct.b));
+    return vec4(color,1.0);
+  }
 
-    gl_FragColor = vec4(color,1.0);
+  vec4 drawRainbow (vec2 st) {
+    vec3 color = vec3(1.0);
+
+    vec3 cViloet = vec3(0.82, 0, 0.97);
+
+    float pViolet = sin(st.x * PI * 0.8 + 0.3) - 0.6;
+    float pIndigo = sin(st.x * PI * 0.8 + 0.3) - 0.5;
+    float pBlue = sin(st.x * PI * 0.8 + 0.3) - 0.4;
+    float pGreen = sin(st.x * PI * 0.8 + 0.3) - 0.3;
+    float pYellow = sin(st.x * PI * 0.8 + 0.3) - 0.2;
+    float pOrane = sin(st.x * PI * 0.8 + 0.3) - 0.1;
+    float pRed = sin(st.x * PI * 0.8 + 0.3);
+    
+    color = mix(color, cViloet, plot(st, pViolet));
+
+    return vec4(color, 1.0);
+  }
+
+  void main() {
+
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
+
+    // vec4 color = drawTurner(st, u_time);
+    vec4 color = drawRainbow(st);
+
+    gl_FragColor = color;
   }
 `;
 
