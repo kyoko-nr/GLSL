9e1acbbca39365adaafae03c401fbad7bebd33fe
kyoko-nr <kyoko_kitagawa@outlook.jp>
2023-10-11 09:40:27 +0900

    added 20

---
+++ 20_noise-2d-1/main.js
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import { setupEnvironment } from "../utils/three/setupEnvironment";
+import { handleWindowSize } from "../utils/three/handleWindowSize";
+import { handleMouse } from "../utils/three/handleMouse";
+
+const vshader = `
+  void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 1.0, 1.0);
+  }
+`;
+
+const fshader = `
+  uniform float u_time;
+  uniform vec2 u_mouse;
+  uniform vec2 u_resolution;
+
+  const float PI = 3.1415926;
+  const float PI2 = PI * 2.0;
+
+  const vec3 cgray = vec3(0.5, 0.5, 0.5);
+  const vec3 cred = vec3(0.9, 0.1, 0.1);
+  const vec3 cblue = vec3(0.3, 0.45, 0.85);
+
+  // 2D Random
+  float random (in vec2 st) {
+      return fract(sin(dot(st.xy,
+                           vec2(12.9898,78.233)))
+                   * 43758.5453123);
+  }
+
+  float noise2D(in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    // Smooth Interpolation
+
+    // Cubic Hermine Curve.  Same as SmoothStep()
+    vec2 u = f*f*(3.0-2.0*f);
+    // u = smoothstep(0.,1.,f);
+
+    // Mix 4 coorners percentages
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+  }
+
+
+  void main() {
+    vec2 st = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+    // vec2 pos = vec2(st.x * abs(sin(u_time * 0.5)) * 15.0, st.y * abs(cos(u_time * 0.5)) * 15.0);
+    vec2 pos = st * 5.0;
+    vec3 color = vec3(0.0);
+
+    color = vec3(noise2D(pos));
+
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
+
+const init = () => {
+  const uniforms = {
+    u_color: {value: new THREE.Color(0xffff00)},
+    u_time: {value: 0.0 },
+    u_mouse: {value: {x: 0.0, y: 0.0}},
+    u_resolution: {value: {x: 0, y: 0}}
+  }
+
+  const {scene, camera, renderer} = setupEnvironment(vshader, fshader, uniforms);
+  handleWindowSize(camera, renderer, uniforms.u_resolution);
+  handleMouse(uniforms.u_mouse);
+
+  const clock = new THREE.Clock();
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    uniforms.u_time.value = clock.getElapsedTime();
+  }
+
+  animate();
+}
+
+document.addEventListener("DOMContentLoaded", init);
